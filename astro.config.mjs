8441169fc8a357f9e25e4d5ee064629e28f98d22
Vicoyes <vicoyes@local>
2026-06-29 20:44:00 +0000

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.liderardesdelaseriedad.com', // Cambia por tu dominio real
+	site: 'https://lideres.transipsicologaintegral.com',
 	integrations: [tailwind()],
 });
